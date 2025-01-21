812e50d4356eb8a352e2ecda334bc34c4e47cd86
Uma Shankar Anantharapu <umashankar.a@digitelenetworks.com>
2025-01-21 18:26:28 +0530

    socket setup done

--- projects/dashboard/src/app/highcharts/highcharts.component.ts
+++ projects/dashboard/src/app/highcharts/highcharts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { HighChartsModule } from '../../../../shared/libs/highcharts.module';
 import { coreModule } from '../../../../shared/libs/core.module';
 import Highcharts from 'highcharts';
@@ -6,6 +6,8 @@ import { Store } from '@ngrx/store';
 import { ChartOptionsState } from '../../../../shared/store/states/state';
 import HighchartsMore from 'highcharts/highcharts-more';
 import ExportingModule from 'highcharts/modules/exporting';
+import { io, Socket } from 'socket.io-client';
+
 
 HighchartsMore
 ExportingModule
@@ -16,35 +18,54 @@ ExportingModule
   templateUrl: './highcharts.component.html',
   styleUrl: './highcharts.component.scss'
 })
-export class HighchartsComponent implements OnChanges {
-    @Input() showRemoveOption!:boolean;
+export class HighchartsComponent implements OnChanges,OnInit {
+  @Input() showRemoveOption!:boolean;
   Highcharts: typeof Highcharts = Highcharts;
+  @Input() chartId!:string;
   @Input() isChartLoaded: boolean = false;
   chart!: ChartOptionsState
-  @Input() chartOptions: Highcharts.Options = {}
-  constructor(private store: Store<{ chartState: ChartOptionsState }>) {
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-    let buttons:any={}
-    if (Highcharts && Highcharts.getOptions()?.exporting?.buttons?.contextButton?.menuItems) {
-      buttons = Highcharts.getOptions().exporting?.buttons?.contextButton?.menuItems?.slice();
-      buttons?.push({
-          text: 'Export to PNG (small)',
-          onclick: function () {
-              this.exportChart({
-                  width: 250,
-              });
-          },
-      });
+  @Input() chartOptions: Highcharts.Options = {};
   
-      // Safely merge and update the menuItems
-      this.chartOptions.exporting = this.chartOptions.exporting || {};
-      this.chartOptions.exporting.buttons = this.chartOptions.exporting.buttons || {};
-      this.chartOptions.exporting.buttons.contextButton = this.chartOptions.exporting.buttons.contextButton || {};
-      this.chartOptions.exporting.buttons.contextButton.menuItems = [
-          ...(this.chartOptions.exporting.buttons.contextButton.menuItems || []),
-          ...buttons,
-      ];
+  private socket!: Socket;
+
+  constructor(private store: Store<{ chartState: ChartOptionsState }>) {
+    this.socket = io('http://localhost:3000'); // WebSocket server URL
+
+  }
+
+  onChartDataUpdate(callback: (data: any) => void) {
+    this.socket.on('chartDataUpdate', callback);
+  }
+
+  // Send updates to the server
+  sendChartUpdate(data: any) {
+    this.socket.emit('updateChart', data);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.showRemoveOption){
+
+      let buttons:any={}
+      if (Highcharts && Highcharts.getOptions()?.exporting?.buttons?.contextButton?.menuItems) {
+        buttons = Highcharts.getOptions().exporting?.buttons?.contextButton?.menuItems?.slice();
+        buttons?.push({
+            text: 'Export to PNG (small)',
+            onclick: function () {
+                this.exportChart({
+                    width: 250,
+                });
+            },
+        });
+    
+        // Safely merge and update the menuItems
+        this.chartOptions.exporting = this.chartOptions.exporting || {};
+        this.chartOptions.exporting.buttons = this.chartOptions.exporting.buttons || {};
+        this.chartOptions.exporting.buttons.contextButton = this.chartOptions.exporting.buttons.contextButton || {};
+        this.chartOptions.exporting.buttons.contextButton.menuItems = [
+            ...(this.chartOptions.exporting.buttons.contextButton.menuItems || []),
+            ...buttons,
+        ];
+    }
   }
   
   // Apply the changes to the chart
@@ -52,9 +73,19 @@ export class HighchartsComponent implements OnChanges {
   
   }
 
-  ngOnInit() {
 
+  ngOnInit() {
+    this.socket.on('updateChart', (data: any[]) => {
+      console.log('Received chart updates:', data);
+      // this.charts = data;
+    });
   }
+
+  ngOnDestroy(): void {
+    // Clean up the socket connection
+    this.socket.disconnect();
+  }
+
   get hasChartOptions(): boolean {
     return this.chartOptions && Object.keys(this.chartOptions).length > 0;
   }
