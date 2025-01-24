e4b4193939a0f20dea0094ce5afb77f0002c2abc
Mounika Kalyadapu <mk@Mounikas-MacBook-Air.local>
2025-01-24 15:24:13 +0530

    initiated graphql first connected

--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-
+import { Apollo } from 'apollo-angular';
+import { GET_DATASET } from './graphQl/dataset.queries';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   host: { 'hostID': crypto.randomUUID().toString() }
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private apollo: Apollo){}
+  ngOnInit() {
+    this.getData()
+  }
+
+  getData(){
+    this.getDataSet().subscribe((res: any) => {
+      console.log(res);
+    })
+    this.apollo.watchQuery({query: GET_DATASET}).valueChanges.subscribe((res: any) => {
+      console.log(res);
+    })
+  }
+  
+  getDataSet(): Observable<any> {
+    return this.apollo.query({
+      query: GET_DATASET,
+      variables: {}
+    })
+  }
   title = 'shell';
 }
--- projects/shell/src/app/app.config.ts
+++ projects/shell/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, inject, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,10 +7,24 @@ import { chartReducer, reducers } from '../../../shared/store/reducers/reducer';
 import { provideStoreDevtools, StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { CommonService } from '../../../shared/common-services/common-service.service';
 import { provideHttpClient } from '@angular/common/http';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     CommonService,
+    provideApollo(() => {
+      const httpLink = inject(HttpLink);
+      return {
+        link: httpLink.create({
+          uri: 'http://10.91.97.160:5000/graphql',
+        }),
+        cache: new InMemoryCache(),
+        connectToDevTools: true
+      };
+    }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore(),
